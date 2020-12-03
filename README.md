c79d9b30cca16caf47d73cb4dec0a8f5e4211e3d
lonnieraymccollister <53101234+lonnieraymccollister@users.noreply.github.com>
2020-12-02 19:08:03 -0600

    initial
    Vernan chipher

---
+++ README.md
@@ -0,0 +1,13 @@
+One Time Pad
+============
+Example
+#### Python
+cd /d E:\lonnielaptop\PythonEnvs\otp\keys
+#### Generates 32,000 keys
+python keygen.py
+cd /d E:\lonnielaptop\PythonEnvs\otp
+enc.py a.txt 0.txt 0a.txt
+dec.py 0a.txt 0.txt a.txt
+
+
+`
---
+++ keygen.py
@@ -0,0 +1,22 @@
+#######################
+# Python XOR 2 Files
+# xor.py
+# Jul 2016
+# Website: http://www.Megabeets.net
+# Use: ./xor.py file1 file2 outputFile
+# Example: ./xor.py C:\a.txt C:\b.txt C:\result.txt
+#######################
+########Modified#######
+
+import sys
+import os
+
+i = 0
+
+# generate otpkeys with random data
+number = 10000
+for i in range(number):
+    file = str(i) + ".txt"
+    newfile = open( file , "wb")
+    newfile.write (os.urandom(100000))    # generate x 1 byte random content file
+    newfile.close ()
