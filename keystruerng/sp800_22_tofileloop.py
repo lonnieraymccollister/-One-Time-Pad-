590811a1405f74b00b395bc8b578bec4d4e48fbd
Lonnie Ray McCollister <53101234+lonnieraymccollister@users.noreply.github.com>
2021-12-10 01:47:25 -0600

    update
    update

--- keystruerng/sp800_22_tofileloop.py
+++ keystruerng/sp800_22_tofileloop.py
@@ -3,8 +3,7 @@ import subprocess
 import shutil, os
 import sys
 
-def loop():
-    i1=i+int(sys.argv[1])
+def loop(i1):
     file = str(i1) + ".txt"
     print(file)
     symfile = ("python sp800_22_tofile.py " + file)
@@ -15,9 +14,9 @@ def loop():
 i=0
 # generate otpkeys with random data
 number = (int(sys.argv[2])+1)-int(sys.argv[1])
-argv1 = int(sys.argv[1])
-for argv1 in range(number):
+for i in range(number):
+    i1=i+int(sys.argv[1])
     starttime = timeit.default_timer()
     print("The start time is :",starttime)
-    loop()
+    loop(i1)
     print("The time difference is :", timeit.default_timer() - starttime)
