9aab36c4b6827be13435e5589ec47828956979d1
Lonnie Ray McCollister <53101234+lonnieraymccollister@users.noreply.github.com>
2023-01-17 04:18:24 -0600

    Update usbrng.py

--- keysusbtruerng_V3/usbrng.py
+++ keysusbtruerng_V3/usbrng.py
@@ -4,7 +4,7 @@ import sys
 
 i = 0
 # generate otpkeys with random data
-number = 25000-int(sys.argv[1])
+number = int(sys.argv[2])-int(sys.argv[1])
 for i in range(number):
     file = str(i+int(sys.argv[1])) + ".txt"
     symfile = ("python usbrng1.py " + str(file))
