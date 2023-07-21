1937605d11eb5476fe148ef11d8a6bd4a21e94f7
Lonnie Ray McCollister <53101234+lonnieraymccollister@users.noreply.github.com>
2023-07-20 20:50:51 -0500

    update
    update

--- keys/Combine8OtpFiles.py
+++ keys/Combine8OtpFiles.py
@@ -5,7 +5,7 @@ import shutil
 from shutil import copyfileobj
 from io import DEFAULT_BUFFER_SIZE
 
-with open(sys.argv[1]+'.txt', 'rb') as input1, open(sys.argv[2]+'.txt', 'rb') as input2, sys.argv[3]+'.txt', 'rb') as input3, open(sys.argv[4]+'.txt', 'rb') as input4, sys.argv[5]+'.txt', 'rb') as input5, open(sys.argv[6]+'.txt', 'rb') as input6, sys.argv[7]+'.txt', 'rb') as input7, open(sys.argv[8]+'.txt', 'rb') as input8, open('output.bin', 'wb') as output:
+with open(sys.argv[1]+'.txt', 'rb') as input1, open(sys.argv[2]+'.txt', 'rb') as input2, open(sys.argv[3]+'.txt', 'rb') as input3, open(sys.argv[4]+'.txt', 'rb') as input4, open(sys.argv[5]+'.txt', 'rb') as input5, open(sys.argv[6]+'.txt', 'rb') as input6, open(sys.argv[7]+'.txt', 'rb') as input7, open(sys.argv[8]+'.txt', 'rb') as input8, open(sys.argv[1]+'_'+sys.argv[2]+'_'+sys.argv[3]+'_'+sys.argv[4]+'_'+sys.argv[5]+'_'+sys.argv[6]+'_'+sys.argv[7]+'_'+sys.argv[8]+'.txt', 'wb') as output:
     copyfileobj(input1, output, DEFAULT_BUFFER_SIZE)
     copyfileobj(input2, output, DEFAULT_BUFFER_SIZE)
     copyfileobj(input3, output, DEFAULT_BUFFER_SIZE)
